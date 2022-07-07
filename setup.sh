45c3e9e437d099b4abbcb0ef1a9a61979ec5e129
Wesley Bliss <wesley.bliss@gmail.com>
2022-07-07 18:19:57 -0500

    Updated setup script to include some missing name/slug replacements

--- setup.sh
+++ setup.sh
@@ -32,39 +32,39 @@ if (!hasName) {
 
 const root = (...dirs) => path.resolve(__dirname, ...dirs)
 
-const read = file => fs.readFileSync(file, 'utf8')
-
-const write = (file, data) => fs.writeFileSync(file, data, 'utf8')
-
-const paths = {
-    me: root('setup.sh'),
-    pkg: root('package.json'),
-    html: root('index.html'),
-    readme: root('README.md'),
-    app: root('src/components/App/App.jsx'),
-    env: root('.env'),
-    sampleEnv: root('sample.env'),
+const replace = target => {
+    
+    console.info('- Updating slug & name:', target)
+    
+    const data = fs.readFileSync(target, 'utf8')
+        .replaceAll('vite-base', slug)
+        .replaceAll('Vite Base', name)
+    
+    fs.writeFileSync(target, data, 'utf8')
+    
 }
 
-const pkg = read(paths.pkg).replace('vite-base', slug)
-const html = read(paths.html).replace('Vite Base', name)
-const readme = read(paths.readme).replace('vite-base', slug)
-const app = read(paths.app).replace('Vite Base', name)
-const sampleEnv = read(paths.sampleEnv).replace('Vite Base', name)
+const paths = [
+    'package.json',
+    'index.html',
+    'README.md',
+    'src/constants.js',
+    'src/components/App/App.jsx',
+    'src/routes/Home.jsx',
+    '.env',
+    'sample.env',
+].map(root)
 
-write(paths.pkg, pkg)
-write(paths.html, html)
-write(paths.readme, readme)
-write(paths.app, app)
-write(paths.sampleEnv, sampleEnv)
+paths.forEach(replace)
 
+console.info('- Creating .env file')
 fs.copyFileSync(paths.sampleEnv, paths.env)
 
+console.info('- Updating files & resetting Git')
 if (fs.existsSync(root('.git'))) {
     fs.rmSync(root('.git'), { recursive: true })
     execSync('git init')
 }
 
-console.info('Updated files & reset Git. Deleting this script')
-
-fs.unlinkSync(paths.me)
+console.info('- Deleting this script')
+fs.unlinkSync(root('setup.sh'))
